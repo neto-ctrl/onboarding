6d79f3f31ba231537d3c75493cb9a0bc10fd3421
Carlos Gomes Moreira Neto <neto20021109@gmail.com>
2024-07-25 20:27:38 -0300

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import styles from "./newpage.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+        <div className="container">  
+          <h1 className="titulo">Vamos compra uma casa</h1> 
+
+
+
+
+        </div>
+      
+      
+    </main>
+  );
+}
