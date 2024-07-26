741239a92ee0e23ecfeff32e55723a071bf4e34a
Carlos Gomes Moreira Neto <neto20021109@gmail.com>
2024-07-25 23:17:13 -0300

    estrutura inicial do html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,38 @@ import styles from "./newpage.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <div className="container">  
-          <h1 className="titulo">Vamos compra uma casa</h1> 
+    <main style={{background:"black", padding:15}}>
+        <div style={{background:"grey", padding:15}} className="container">  
+          <span>Vamos compra uma casa</span> 
+             <div style={{display:"flex",flexDirection:"column", backgroundColor:"red", padding:20, gap:20 }}>
+              <div style={{display:"flex", gap:5}}>
+              <div style={{width:50, height:50, background:"blue"}} /> 
+              
+                <div style={{display:"flex", flexDirection:"column"}}>
+                  
+                  <span> Comprar uma casa</span>
+                  <span> meta de economia</span>
+                </div>
 
+    
+              </div>
+              <div style={{display:"flex", justifyContent:"space-between"}}>
+                <span> valor</span>
+                <span>data</span>
+
+              </div>
+
+              <div style={{ display:"flex", flexDirection:"column", gap:20 }}>
+                <div  style={{display:"flex", justifyContent:"space-around"}}>
+                  <span>economia mensal</span>
+                  <span> valor</span>
+                </div>
+                <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+                  <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cupiditate veniam dolorem modi odio impedit nesciunt qui aliquam minima doloremque. Aperiam quaerat quod sunt laudantium dolorum voluptates ipsum necessitatibus eveniet!</span>
+                </div>
+
+              </div>
+             </div>
 
 
 
